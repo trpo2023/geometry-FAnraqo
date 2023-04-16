ba35ea18265969fa6fd578c4469cd64f1c3a41ed
FAnraqo <vs045920@gmail.com>
2023-04-16 23:29:01 +0700

    Fix functions.h

--- src/libgeometry/functions.h
+++ src/libgeometry/functions.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
 int checking_for_errors(string temp);
 void filling_in_structure(string temp, float *x, float *y, float *r, int k);
 void calculation(float *x, float *y, float *r, float *perimeter, float *area, int k);
