70a4a9af1f5ed8ee9b940ca8bf24cbb1b43432c9
FAnraqo <vs045920@gmail.com>
2023-04-17 08:56:27 +0700

    Fix codestyle main.cpp

--- src/geometry/main.cpp
+++ src/geometry/main.cpp
@@ -44,7 +44,8 @@ int main()
     if (flag != 0)
     {
         printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        printf("\nВ файле есть ошибки в оформлении данных, исправьте их и запустите программу заново)\n");
+        printf("\nВ файле есть ошибки в оформлении данных");
+        printf(", исправьте их и запустите программу заново)\n");
         f.close();
         return 0;
     }
@@ -65,21 +66,36 @@ int main()
         {
             string temp;
             getline(f, temp);
-            filling_in_structure(temp, &fgr[k].x, &fgr[k].y, &fgr[k].r, k);
+            filling_in_structure(temp, &fgr[k].x, &fgr[k].y, &fgr[k].r);
             k ++;
         }
 
     for (int i = 0; i < k; i++)
     {
-        calculation(&fgr[i].x, &fgr[i].y, &fgr[i].r, &fgr[i].perimeter, &fgr[i].area, i);
+        calculation(&fgr[i].r, &fgr[i].perimeter, &fgr[i].area);
         for (int j = 0; j < k; j++)
             if (i != j)
-                overlay(&fgr[i].x, &fgr[i].y, &fgr[i].r, &fgr[i].intersects, &fgr[j].x, &fgr[j].y, &fgr[j].r, j);
+                overlay(
+                    &fgr[i].x, 
+                    &fgr[i].y, 
+                    &fgr[i].r, 
+                    &fgr[i].intersects, 
+                    &fgr[j].x, 
+                    &fgr[j].y, 
+                    &fgr[j].r, 
+                    j);
     }
 
     for (int i = 0; i < k; i++)
     {
-        print_structure(&fgr[i].x, &fgr[i].y, &fgr[i].r, &fgr[i].perimeter, &fgr[i].area, &fgr[i].intersects, i);
+        print_structure(
+            &fgr[i].x, 
+            &fgr[i].y, 
+            &fgr[i].r, 
+            &fgr[i].perimeter, 
+            &fgr[i].area, 
+            &fgr[i].intersects, 
+            i);
     }
     
     return 0;
