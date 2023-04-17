387d2cd7cfc5f503aa5a77eb5b3121b1a7fbdef3
FAnraqo <vs045920@gmail.com>
2023-04-17 08:57:27 +0700

    Fix codestyle functions.h

--- src/libgeometry/functions.h
+++ src/libgeometry/functions.h
@@ -7,7 +7,22 @@
 using namespace std;
 
 int checking_for_errors(string temp);
-void filling_in_structure(string temp, float *x, float *y, float *r, int k);
-void calculation(float *x, float *y, float *r, float *perimeter, float *area, int k);
-void overlay(float *xi, float *yi, float *ri, string *intersects, float *xj, float *yj, float *rj, int j);
-void print_structure(float *x, float *y, float *r, float *perimeter, float *area, string *intersects, int i);
+void filling_in_structure(string temp, float* x, float* y, float* r);
+void calculation(float* r, float* perimeter, float* area);
+void overlay(
+        float* xi,
+        float* yi,
+        float* ri,
+        string* intersects,
+        float* xj,
+        float* yj,
+        float* rj,
+        int j);
+void print_structure(
+        float* x,
+        float* y,
+        float* r,
+        float* perimeter,
+        float* area,
+        string* intersects,
+        int i);
