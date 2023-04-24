1865ab1c87a41e4ec36a8a56e55fbb20113fb47b
FAnraqo <vs045920@gmail.com>
2023-04-24 08:51:43 +0700

    Fix codestyle main.cpp

--- src/geometry/main.cpp
+++ src/geometry/main.cpp
@@ -1,12 +1,11 @@
 #include <libgeometry/functions.h>
 
+#include <fstream>
 #include <iostream>
 #include <stdio.h>
-#include <fstream>
 #include <string>
 
-struct figure
-{
+struct figure {
     float x = 0;
     float y = 0;
     float r = 0;
@@ -25,22 +24,19 @@ int main()
     int k = 0;
     int flag = 0;
 
-    if (!f.is_open())
-    {
+    if (!f.is_open()) {
         std::cout << "Файл не открыт" << std::endl;
         return 0;
-    } 
-    else
-        while (!f.eof())
-        {
+    } else
+        while (!f.eof()) {
             std::string temp;
             getline(f, temp);
-            if (checking_for_errors(temp) != 0) flag = 1;
-            k ++;
+            if (checking_for_errors(temp) != 0)
+                flag = 1;
+            k++;
         }
 
-    if (flag != 0)
-    {
+    if (flag != 0) {
         printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         printf("\nВ файле есть ошибки в оформлении данных");
         printf(", исправьте их и запустите программу заново)\n");
@@ -52,49 +48,43 @@ int main()
 
     figure fgr[k];
     k = 0;
-    
+
     f.open(file);
-    if (!f.is_open())
-    {
+    if (!f.is_open()) {
         std::cout << "Файл не открыт" << std::endl;
         return 0;
-    } 
-    else
-        while (!f.eof())
-        {
+    } else
+        while (!f.eof()) {
             std::string temp;
             getline(f, temp);
             filling_in_structure(temp, &fgr[k].x, &fgr[k].y, &fgr[k].r);
-            k ++;
+            k++;
         }
 
-    for (int i = 0; i < k; i++)
-    {
+    for (int i = 0; i < k; i++) {
         calculation(&fgr[i].r, &fgr[i].perimeter, &fgr[i].area);
         for (int j = 0; j < k; j++)
             if (i != j)
-                overlay(
-                    &fgr[i].x, 
-                    &fgr[i].y, 
-                    &fgr[i].r, 
-                    &fgr[i].intersects, 
-                    &fgr[j].x, 
-                    &fgr[j].y, 
-                    &fgr[j].r, 
-                    j);
+                overlay(&fgr[i].x,
+                        &fgr[i].y,
+                        &fgr[i].r,
+                        &fgr[i].intersects,
+                        &fgr[j].x,
+                        &fgr[j].y,
+                        &fgr[j].r,
+                        j);
     }
 
-    for (int i = 0; i < k; i++)
-    {
+    for (int i = 0; i < k; i++) {
         print_structure(
-            &fgr[i].x, 
-            &fgr[i].y, 
-            &fgr[i].r, 
-            &fgr[i].perimeter, 
-            &fgr[i].area, 
-            &fgr[i].intersects, 
-            i);
+                &fgr[i].x,
+                &fgr[i].y,
+                &fgr[i].r,
+                &fgr[i].perimeter,
+                &fgr[i].area,
+                &fgr[i].intersects,
+                i);
     }
-    
+
     return 0;
 }
